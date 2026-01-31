637443a465045eae18f0ce73b5ec8915f655346d
jiajin0329 <98139948+jiajin0329@users.noreply.github.com>
2026-01-31 16:00:45 +0800

    Backup.

--- GGJ2026-Taipei-TeamD/Assets/_TeamD/_Scripts/TimerView.cs
+++ GGJ2026-Taipei-TeamD/Assets/_TeamD/_Scripts/TimerView.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 namespace WhoIsCatchingNaps
 {
     [Serializable]
-    public struct LevelManagerView
+    public struct TimerView
     {
         [SerializeField]
         private TextMeshProUGUI _timerText;
 
         public void SetTimerText(int _timer)
         {
-            _timerText.text = _timer.ToString();
+            int _min = _timer / 60;
+            int _sec = _timer - (_min * 60);
+
+            _timerText.text = $"{_min}:{_sec.ToString("D2")}" ;
         }
     }
 }
