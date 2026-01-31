9b979cfb72e22406d887ff186b05b39a46dc2ef9
jiajin0329 <98139948+jiajin0329@users.noreply.github.com>
2026-01-31 18:27:07 +0800

    ✨New: 新增減時間效果。

--- Assets/_TeamD/_Scripts/Game/LevelSettings.cs
+++ Assets/_TeamD/_Scripts/Game/LevelSettings.cs
@@ -6,6 +6,21 @@ namespace WhoIsCatchingNaps
     public class LevelSettings : ScriptableObject
     {
         [field: SerializeField]
-        public int levelTime;
+        public int levelTime { get; private set; } = 120;
+
+        [field: SerializeField]
+        public float reduceTime { get; private set; }
+
+        [field: SerializeField]
+        public Color reduceTimeColor { get; private set; }
+
+        [field: SerializeField]
+        public float reduceTimeShakeStrength { get; private set; } = 50f;
+
+        [field: SerializeField]
+        public int reduceTimeShakeVibrato { get; private set; } = 100;
+
+        [field: SerializeField]
+        public float reduceTimeTextMoveY { get; private set; } = 50f;
     }
 }
--- Assets/_TeamD/_Scripts/Game/TimerView.cs
+++ Assets/_TeamD/_Scripts/Game/TimerView.cs
@@ -1,14 +1,30 @@
 using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 namespace WhoIsCatchingNaps
 {
     [Serializable]
-    public struct TimerView
+    public class TimerView
     {
         [SerializeField]
         private TextMeshProUGUI _timerText;
+
+        [SerializeField]
+        private GameObject _reduceTimeText;
+
+        private LevelSettings _levelSettings;
+        private Color _startColor;
+
+
+        public void Initialize(LevelSettings _levelSettings)
+        {
+            this._levelSettings = _levelSettings;
+            _startColor = _timerText.color;
+            _reduceTimeText.SetActive(false);
+        }
 
         public void SetTimerText(int _timer)
         {
@@ -16,6 +32,27 @@ namespace WhoIsCatchingNaps
             int _sec = _timer - (_min * 60);
 
             _timerText.text = $"{_min}:{_sec.ToString("D2")}" ;
+        }
+
+        public async UniTaskVoid Reduce(float _reduce)
+        {
+            ReduceTimeTextEffect();
+
+            _timerText.DOColor(_levelSettings.reduceTimeColor, 0.25f);
+            
+            float _shakeDuration = 0.25f;
+            _timerText.transform.DOShakePosition(_shakeDuration, _levelSettings.reduceTimeShakeStrength, _levelSettings.reduceTimeShakeVibrato);
+            await UniTask.Delay((int)(_shakeDuration*1000f));
+
+            _timerText.DOColor(_startColor, 0.25f);
+        }
+
+        private void ReduceTimeTextEffect()
+        {
+            var _clone = UnityEngine.Object.Instantiate(_reduceTimeText, _reduceTimeText.transform.parent);
+            var _cloneTransform = _clone.transform;
+            _cloneTransform.DOLocalMoveY(_cloneTransform.localPosition.y + _levelSettings.reduceTimeTextMoveY, 1f).OnComplete(() => UnityEngine.Object.Destroy(_clone));
+            _clone.gameObject.SetActive(true);
         }
     }
 }
